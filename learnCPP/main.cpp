a0fa4cf4163bcf3139f6f6b07b7b0bd9767099c2
ZhangZidong <2018zhangzd@gmail.com>
2018-10-30 00:15:41 -0400

    Initial Commit

---
+++ learnCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  learnCPP
+//
+//  Created by 张子洞 on 10/30/18.
+//  Copyright © 2018 张子洞. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
