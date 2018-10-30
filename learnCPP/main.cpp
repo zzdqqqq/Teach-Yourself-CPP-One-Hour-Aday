213db436745588825291182017d5ae7431a11e90
ZhangZidong <2018zhangzd@gmail.com>
2018-10-30 01:54:42 -0400

    review

--- learnCPP/main.cpp
+++ learnCPP/main.cpp
@@ -7,9 +7,35 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
+using namespace std;
+void DemoConsoleOutput();
+void Demo2();
+void Demo3();
+int main() {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    DemoConsoleOutput();
+    Demo2();
+    Demo3();
     return 0;
+    
+}
+void Demo3(){
+    
+}
+void Demo2(){
+    int inputNumber;
+    
+    cout << "Enter an integer:\n";
+    cin >> inputNumber;
+    
+    cout << inputNumber << endl;
+}
+
+void DemoConsoleOutput(){
+    cout << "This is a simple string literal" << endl;
+    cout << "Writing number five:" << endl;
+    int x = 8;
+    int y = 6;
+    cout << "Hello, World!\n";
+    cout << x - y << " " << x * y << " " << x + y << endl;
 }
