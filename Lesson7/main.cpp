0f43fcaa27476c866d5bd7b545a506be17eed1c7
ZhangZidong <2018zhangzd@gmail.com>
2019-01-10 14:50:12 -0500

    From lesson 7, I know how to create a new execute document to build some other code

---
+++ Lesson7/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lesson7
+//
+//  Created by Zidong Zhang on 1/10/19.
+//  Copyright © 2019 张子洞. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
